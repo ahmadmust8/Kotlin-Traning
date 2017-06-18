8456e4ad03101f582c77c3fccca0e4a00a0e1e7d
Ahmad Mostafa <ahmadmust8@gmail.com>
2017-06-19 02:06:40 +0300

    learn When Stetement & Range of Variables

---
+++ src/RangeOfVariables.kt
@@ -0,0 +1,32 @@
+/**
+ * Created by ahmad on 19/06/17.
+ */
+
+fun main(args:Array<String>){
+
+    println("Byte maxValue : " +Byte.MAX_VALUE +" minValue : " + Byte.MIN_VALUE )
+
+    println("Int maxValue : " +Int.MAX_VALUE +" minValue : " + Int.MIN_VALUE )
+
+    println("Double maxValue : " +Double.MAX_VALUE +" minValue : " + Double.MIN_VALUE )
+
+    println("Long maxValue : " +Long.MAX_VALUE +" minValue : " + Long.MIN_VALUE )
+
+    println("Float maxValue : " +Float.MAX_VALUE +" minValue : " + Float.MIN_VALUE )
+
+    println("Short maxValue  : " +Short.MAX_VALUE +" minValue : " + Short.MIN_VALUE )
+
+    /**
+     * variables Ranges
+     *
+     * Byte from : 127 to -128
+     * Int from : 2147483647 to -2147483648
+     * Double from : 1.7976931348623157E308 to 4.9E-324
+     * Long from : 9223372036854775807 to -9223372036854775808
+     * Float from : 3.4028235E38 to 1.4E-45
+     * Short from : 32767 to -32768
+     */
+
+
+
+}
---
+++ src/WhenStatement.kt
@@ -0,0 +1,28 @@
+/**
+ * Created by ahmad on 19/06/17.
+ */
+
+fun main(args:Array<String>){
+
+    print("Enter Number : ")
+    var num = readLine()?.toLong()
+    // the when statement in Kotlin is like S
+    when(num){
+
+        55525255585->
+                println("Zero")
+
+        55858555555->
+                println("One")
+
+        100000000000->
+                println("Tow")
+
+        2123123112313212->
+                println("Hello Ahmad, how it's going?")
+
+        else->
+                println("Other Number....")
+
+    }
+}
